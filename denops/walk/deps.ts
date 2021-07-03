444b43540eea092a9d5c0810336756799857635b
yukimemi <yukimemi@gmail.com>
2021-07-03 23:28:57 +0900

    :up: Update denops_std.

--- denops/walk/deps.ts
+++ denops/walk/deps.ts
@@ -3,7 +3,7 @@ export * as path from "https://deno.land/std@0.100.0/path/mod.ts";
 export * as flags from "https://deno.land/std@0.100.0/flags/mod.ts";
 export * as fs from "https://deno.land/std@0.100.0/fs/mod.ts";
 
-export * as autocmd from "https://deno.land/x/denops_std@v1.0.0-alpha.0/autocmd/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v1.0.0-alpha.0/variable/mod.ts";
-export { Denops } from "https://deno.land/x/denops_std@v1.0.0-alpha.0/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v1.0.0-alpha.0/helper/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-alpha.3/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v1.0.0-alpha.3/autocmd/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v1.0.0-alpha.3/variable/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v1.0.0-alpha.3/helper/mod.ts";
