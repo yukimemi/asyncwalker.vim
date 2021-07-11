e40cbb55d5ed8d6f512f6450a2610f60a231a214
yukimemi <yukimemi@gmail.com>
2021-07-11 22:34:57 +0900

    :up: Update denops_std.

--- denops/walk/deps.ts
+++ denops/walk/deps.ts
@@ -3,8 +3,8 @@ export * as path from "https://deno.land/std@0.100.0/path/mod.ts";
 export * as flags from "https://deno.land/std@0.100.0/flags/mod.ts";
 export * as fs from "https://deno.land/std@0.100.0/fs/mod.ts";
 
-export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-beta.4/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v1.0.0-beta.4/autocmd/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v1.0.0-beta.4/variable/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v1.0.0-beta.4/function/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v1.0.0-beta.4/helper/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-beta.5/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v1.0.0-beta.5/autocmd/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v1.0.0-beta.5/variable/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v1.0.0-beta.5/function/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v1.0.0-beta.5/helper/mod.ts";
