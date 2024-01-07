1a5a3aef444f33cd688b17da613bcdcdc89e3c3b
GitHub <noreply@github.com>
2024-01-07 06:04:49 +0000

    chore(deps): Update deno dependencies

--- denops/walk/main.ts
+++ denops/walk/main.ts
@@ -7,7 +7,7 @@ import * as path from "https://deno.land/std@0.211.0/path/mod.ts";
 import * as vars from "https://deno.land/x/denops_std@v5.2.0/variable/mod.ts";
 import * as buffer from "https://deno.land/x/denops_std@v5.2.0/buffer/mod.ts";
 import type { Denops } from "https://deno.land/x/denops_std@v5.2.0/mod.ts";
-import { ensure, is } from "https://deno.land/x/unknownutil@v3.11.0/mod.ts";
+import { ensure, is } from "https://deno.land/x/unknownutil@v3.13.0/mod.ts";
 import { batch } from "https://deno.land/x/denops_std@v5.2.0/batch/mod.ts";
 import { echo, echoerr, execute, input } from "https://deno.land/x/denops_std@v5.2.0/helper/mod.ts";
 import { send } from "https://deno.land/x/denops_std@v5.2.0/helper/keymap.ts";
