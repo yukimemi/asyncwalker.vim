5d3b04df0ec7fd7d3dcf60062e481759cbe5c5c3
GitHub <noreply@github.com>
2024-02-15 06:05:11 +0000

    chore(deps): Update deno dependencies

--- denops/walk/main.ts
+++ denops/walk/main.ts
@@ -1,9 +1,9 @@
 import * as _ from "https://cdn.skypack.dev/lodash@4.17.21";
 import * as autocmd from "https://deno.land/x/denops_std@v6.0.1/autocmd/mod.ts";
-import * as flags from "https://deno.land/std@0.215.0/flags/mod.ts";
+import * as flags from "https://deno.land/std@0.216.0/flags/mod.ts";
 import * as fn from "https://deno.land/x/denops_std@v6.0.1/function/mod.ts";
-import * as fs from "https://deno.land/std@0.215.0/fs/mod.ts";
-import * as path from "https://deno.land/std@0.215.0/path/mod.ts";
+import * as fs from "https://deno.land/std@0.216.0/fs/mod.ts";
+import * as path from "https://deno.land/std@0.216.0/path/mod.ts";
 import * as vars from "https://deno.land/x/denops_std@v6.0.1/variable/mod.ts";
 import * as buffer from "https://deno.land/x/denops_std@v6.0.1/buffer/mod.ts";
 import type { Denops } from "https://deno.land/x/denops_std@v6.0.1/mod.ts";
