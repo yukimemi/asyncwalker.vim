94587dfd9e7a5bcb98021ffe61d0347bc9955af2
GitHub <noreply@github.com>
2024-05-15 06:05:16 +0000

    chore(deps): Update deno dependencies

--- denops/scanwalker/main.ts
+++ denops/scanwalker/main.ts
@@ -1,15 +1,15 @@
 import * as _ from "https://cdn.skypack.dev/lodash@4.17.21";
-import * as autocmd from "https://deno.land/x/denops_std@v6.4.3/autocmd/mod.ts";
+import * as autocmd from "https://deno.land/x/denops_std@v6.5.0/autocmd/mod.ts";
 import * as flags from "https://deno.land/std@0.224.0/flags/mod.ts";
-import * as fn from "https://deno.land/x/denops_std@v6.4.3/function/mod.ts";
+import * as fn from "https://deno.land/x/denops_std@v6.5.0/function/mod.ts";
 import * as fs from "https://deno.land/std@0.224.0/fs/mod.ts";
 import * as path from "https://deno.land/std@0.224.0/path/mod.ts";
-import * as vars from "https://deno.land/x/denops_std@v6.4.3/variable/mod.ts";
-import * as buffer from "https://deno.land/x/denops_std@v6.4.3/buffer/mod.ts";
-import type { Denops } from "https://deno.land/x/denops_std@v6.4.3/mod.ts";
+import * as vars from "https://deno.land/x/denops_std@v6.5.0/variable/mod.ts";
+import * as buffer from "https://deno.land/x/denops_std@v6.5.0/buffer/mod.ts";
+import type { Denops } from "https://deno.land/x/denops_std@v6.5.0/mod.ts";
 import { ensure, is } from "https://deno.land/x/unknownutil@v3.18.1/mod.ts";
-import { batch } from "https://deno.land/x/denops_std@v6.4.3/batch/mod.ts";
-import { echo, echoerr, execute, input } from "https://deno.land/x/denops_std@v6.4.3/helper/mod.ts";
+import { batch } from "https://deno.land/x/denops_std@v6.5.0/batch/mod.ts";
+import { echo, echoerr, execute, input } from "https://deno.land/x/denops_std@v6.5.0/helper/mod.ts";
 
 let entries: string[] = [];
 let filterEntries: string[] = [];
